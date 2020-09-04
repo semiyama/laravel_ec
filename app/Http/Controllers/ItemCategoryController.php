9fb59e88f890c47801b6a58a115066077bf0a6af
sakamotoHome <shusaku.sakamoto@soranoshita.net>
2020-09-04 13:50:40 +0900

    laravel shop　初コミット

---
+++ app/Http/Controllers/ItemCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\item_category;
+use Illuminate\Http\Request;
+
+class ItemCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\item_category  $item_category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(item_category $item_category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\item_category  $item_category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(item_category $item_category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\item_category  $item_category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, item_category $item_category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\item_category  $item_category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(item_category $item_category)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device=width, initial-scale=1, shrink-fit=no'>
+  <meta name='csrf-token' content='{{ csrf_token() }}'>
+
+  <title>テストECサイト</title>
+</head>
+<body>
+  <table>
+  @foreach($items as $item)
+  <tr>
+    <td>{{ $item->name }}</td>
+    <td>
+      <a href="">
+        {{ $item->name }}
+      </a>
+    </td>
+    <td>カテゴリ番号{{ $item->category }}</td>
+  </tr>
+  @endforeach
+</table>
+</body>
+</html>
