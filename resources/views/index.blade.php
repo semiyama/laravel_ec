1b3465086a2e4756ba7e7e8f58bd44f8d9afed83
sakamotoHome <shusaku.sakamoto@soranoshita.net>
2020-09-04 14:12:37 +0900

    node module 削除前

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device=width, initial-scale=1, shrink-fit=no'>
   <meta name='csrf-token' content='{{ csrf_token() }}'>
-
+  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
   <title>テストECサイト</title>
 </head>
 <body>
