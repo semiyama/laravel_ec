8008642019d2a4991f630d003bf209a693909389
sakamotoHome <shusaku.sakamoto@soranoshita.net>
2020-09-18 08:06:41 +0900

    コーディング一通り完成（レスポンシブは後回し）

--- resources/views/admin/item_detail.blade.php
+++ resources/views/admin/item_detail.blade.php
@@ -7,18 +7,68 @@
   <meta name="robots" content="noindex">
   <link href="{{ asset('/css/reset.css') }}" rel="stylesheet">
   <link href="{{ asset('/css/common.css') }}" rel="stylesheet">
-  <link href="{{ asset('/css/admin/login.css') }}" rel="stylesheet">
-  <title>ログイン｜Laravel サンプルECサイト</title>
+  <link href="{{ asset('/css/admin/order_detail.css') }}" rel="stylesheet">
+  <title>受注詳細｜Laravel ECサイト サンプル管理画面</title>
 </head>
 <body>
   <header>Laravel ECサイト サンプル管理画面</header>
 
   <div class="main">
-    <form>
-      <input type="text">
-      <input type="text">
-      <button>ログイン</button>
-    </form>
+    <h1>受注詳細</h1>
+
+    <div class="container">
+
+      <section class="basic">
+        <h2>基本情報</h2>
+
+        <table>
+          <tr>
+            <th>注文ID 120</th>
+          </tr>
+          <tr>
+            <th>商品名</th>
+            <td>
+              <input type="text" name="name">
+            </td>
+          </tr>
+          <tr>
+            <th>カテゴリ</th>
+            <td class="address">
+              <select>
+                <option>選択して下さい</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <th>説明文</th>
+            <td class="memo">
+              <textarea></textarea>
+            </td>
+          </tr>
+          <tr>
+            <th>税別価格</th>
+            <td>
+              <input type="text" name="price">
+            </td>
+          </tr>
+        </table>
+      </section>
+
+      <a class="updateBtn">更新</a>
+
+
+    </div>
+
   </div>
+
+  <!-- 左サイドバー -->
+  <div id="side">
+    <ul>
+      <li><a href="">ホーム</a></li>
+      <li><a href="">商品管理</a></li>
+      <li><a href="">受注管理</a></li>
+    </ul>
+  </div>
+
 </body>
 </html>
--- resources/views/admin/item_list.blade.php
+++ resources/views/admin/item_list.blade.php
@@ -7,18 +7,81 @@
   <meta name="robots" content="noindex">
   <link href="{{ asset('/css/reset.css') }}" rel="stylesheet">
   <link href="{{ asset('/css/common.css') }}" rel="stylesheet">
-  <link href="{{ asset('/css/admin/login.css') }}" rel="stylesheet">
-  <title>ログイン｜Laravel サンプルECサイト</title>
+  <link href="{{ asset('/css/admin/item_list.css') }}" rel="stylesheet">
+  <title>受注一覧｜Laravel ECサイト サンプル管理画面</title>
 </head>
 <body>
   <header>Laravel ECサイト サンプル管理画面</header>
 
   <div class="main">
-    <form>
-      <input type="text">
-      <input type="text">
-      <button>ログイン</button>
-    </form>
+    <h1>受注一覧</h1>
+
+    <div class="container">
+      <div class="searchPanel">
+        <table>
+          <tr>
+            <td>
+              商品ID<br>
+              <input type="text" name="id">
+            </td>
+            <td>
+              カテゴリ<br>
+              <select name="category">
+                <option>選択して下さい</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              商品名<br>
+              <input type="text" name="name">
+            </td>
+          </tr>
+
+        </table>
+
+        <div class="clearBtnBox">
+          <a>検索条件をクリア</a>
+        </div>
+      </div>
+
+      <a class="searchBtn">検索する</a>
+
+      {{-- アイテム一覧 --}}
+      <div class="itemList">
+        <div>
+          <button>新規追加</button>
+        </div>
+        <table>
+          <tr>
+            <th>商品ID</th>
+            <th>商品名</th>
+            <th>カテゴリ</th>
+            <th>単価</th>
+            <th></th>
+          </tr>
+
+          <tr>
+            <td>120</td>
+            <td>阪本周作</td>
+            <td>本</td>
+            <td>120,000円</td>
+            <td>編集</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
   </div>
+
+  <!-- 左サイドバー -->
+  <div id="side">
+    <ul>
+      <li><a href="">ホーム</a></li>
+      <li><a href="">商品管理</a></li>
+      <li><a href="">受注管理</a></li>
+    </ul>
+  </div>
+
 </body>
 </html>
